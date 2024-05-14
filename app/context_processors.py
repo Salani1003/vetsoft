23ece4b48f377168b359e18b5a253b3fefa3edac
MorettiFrancisco <franmoretti02@hotmail.com>
2024-05-14 17:30:09 -0300

    Added navigation through icons in the Navbar

--- app/context_processors.py
+++ app/context_processors.py
@@ -3,6 +3,12 @@ from django.urls import reverse
 links = [
     {"label": "Home", "href": reverse("home"), "icon": "bi bi-house-door"},
     {"label": "Clientes", "href": reverse("clients_repo"), "icon": "bi bi-people"},
+    {"label": "Produtos", "href": reverse("products_repo"), "icon": "bi bi-box"},
+    {"label": "Mascotas", "href": reverse("pets_repo"), "icon": "bi bi-paw"},
+    {"label": "Veterinarios", "href": reverse("vets_repo"), "icon": "bi bi-person"},
+    {"label": "Citas", "href": reverse("appointments_repo"), "icon": "bi bi-calendar"},
+    {"label": "Medicamentos", "href": reverse("medicines_repo"), "icon": "bi bi-pill"},
+    {"label": "Proveedores", "href": reverse("providers_repo"), "icon": "bi bi-truck"},
 ]
 
 
