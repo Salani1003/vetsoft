4484b11538b14981aecf561dd87ccced427770dc
tomasz <zubik.tomas@gmail.com>
2024-06-03 20:05:59 -0300

    fix: set render ip to csrf

--- vetsoft/settings.py
+++ vetsoft/settings.py
@@ -119,7 +119,7 @@ USE_I18N = True
 USE_TZ = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://vetsoft-grupo-8.onrender.com"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
